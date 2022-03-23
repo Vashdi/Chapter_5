8470f65c7db6f4fabfe5a666945747503653192e
Vashdi <vashdi7002@gmail.com>
2022-03-23 18:42:02 +0200

    change

--- Todo_app/services/todoAppBackend/mongoDB/Connect.js
+++ Todo_app/services/todoAppBackend/mongoDB/Connect.js
@@ -1,11 +1,11 @@
 const mongooseConnection = () => {
   const mongoose = require("mongoose");
-  const MONGODB_URI =
+  const MONGODB_URL =
     process.env.NODE_ENV === "test"
       ? process.env.TEST_MONGODB_URI
-      : process.env.MONGODB_URI;
+      : process.env.MONGODB_URL;
 
-  mongoose.connect(MONGODB_URI);
+  mongoose.connect(MONGODB_URL);
 };
 
 module.exports = { mongooseConnection };
