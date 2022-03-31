af565c43109326efeb0865ee9bc0e9d32cca49ff
Vashdi <vashdi7002@gmail.com>
2022-03-31 13:29:27 +0300

    fix

--- Todo_app/src/redux/actions/Action.js
+++ Todo_app/src/redux/actions/Action.js
@@ -9,7 +9,7 @@ export const toggleShow = (doShowAll) => {
   };
 };
 
-export const toggleSortAZ = (doSortAZ, allTasks, mutate) => {
+export const toggleSortAZ = (doSortAZ) => {
   return async (dispatch) => {
     dispatch({ type: SORT_AZ, payload: !doSortAZ });
   };
