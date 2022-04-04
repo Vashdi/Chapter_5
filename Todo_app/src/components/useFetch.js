638e1b4d857c5ab72d5df8aa7ae8c68e50a41339
Vashdi <vashdi7002@gmail.com>
2022-04-04 12:00:37 +0300

    fix useFetch

--- Todo_app/src/components/useFetch.js
+++ Todo_app/src/components/useFetch.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import configService from "./config.js";
 
-export default function useFetch(showAll) {
+export default function useFetch(url, showAll) {
   const [tasks, setTasks] = useState([]);
 
   useEffect(async () => {
-    const optionAxios = `doShowAll=${showAll}`;
-    const res = await axios.get(configService.todo_api + `?${optionAxios}`);
-    let dbAllTasks = res.data;
+    //const optionAxios = `doShowAll=${showAll}`;
+    //url = configService.todo_api + `?${optionAxios}`
+    const res = await axios.get(url);
+    const dbAllTasks = res.data;
     setTasks(dbAllTasks);
   }, [showAll]);
 
